4feb2ca2ef7d9b466fe24a99b8a28cba509aea48
takuminakayama <143808505+zhongshanzuoxin@users.noreply.github.com>
2023-10-02 08:43:43 +0000

    [Finish]4-7c

---
+++ app/models/group.rb
@@ -0,0 +1,14 @@
+class Group < ApplicationRecord
+  
+  has_many :group_users, dependent: :destroy
+  belongs_to :owner, class_name: 'User'
+  has_many :users, through: :group_users
+  
+  validates :name, presence: true
+  validates :introduction, presence: true
+  has_one_attached :group_image
+  
+  def is_owned_by?(user)
+    owner.id == user.id
+  end 
+end
